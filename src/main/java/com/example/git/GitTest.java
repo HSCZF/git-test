2c5ba230fb35ab5b265c3e7ba357afaaf44d78a0
HSCZF <1337081497@qq.com>
2021-06-03 19:19:00 +0800

    hot-fix commit2

--- src/main/java/com/example/git/GitTest.java
+++ src/main/java/com/example/git/GitTest.java
@@ -7,6 +7,7 @@ public class GitTest {
         System.out.println("hello git2!");
         System.out.println("hello git3!");
         System.out.println("hello git4!");
+        System.out.println("hot-fix test!");
 
     }
 
