5aa45b19842d040f5a27b5eac6238e07ccd20ca3
HSCZF <1337081497@qq.com>
2021-06-03 19:51:17 +0800

    push commit

--- src/main/java/com/example/git/GitTest.java
+++ src/main/java/com/example/git/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
         System.out.println("hello git4!");
         System.out.println("master test!");
         System.out.println("hot-fix test!");
+        System.out.println("push test!");
 
     }
 
